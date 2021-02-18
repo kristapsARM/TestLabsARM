2a2c68ca57a919a676ab24840d1ca9a62a2ddf33
Kristaps Pelcbergs <kristaps.pelcbergs@testdevlab.com>
2021-02-18 15:15:55 +0200

    first commit

---
+++ Exchange/CSDownloadISO.ps1
@@ -0,0 +1,98 @@
+param (
+	[Parameter(Mandatory)]
+    [string]$uri,
+	[Parameter(Mandatory)]
+    [string]$destination,
+    [Parameter(Mandatory)]
+    [string]$domainname
+)
+
+function DownloadISO {
+
+	# Local file storage location
+    $localPath = "$env:SystemDrive"
+    
+
+    # Log file
+    $logFileName = "CSDownload.log"
+    $logFilePath = "$localPath\$logFileName"
+	
+	if(Test-Path $destination) {
+		"Destination path exists. Skipping ISO download" | Tee-Object -FilePath $logFilePath -Append
+		return
+	}
+	
+	$destination = Join-Path $env:SystemDrive $destination
+	New-Item -Path $destination -ItemType Directory
+
+    $installPath = "$env:PUBLIC\Desktop\Hybrid"
+    New-Item -Path $installPath -ItemType Directory -Force 
+
+
+
+	$destinationFile = $null
+    $result = $false
+	# Download ISO
+	$retries = 3
+	# Stop retrying after download succeeds or all retries attempted
+	while(($retries -gt 0) -and ($result -eq $false)) {
+		try
+		{
+			"Downloading ISO from URI: $uri to destination: $destination" | Tee-Object -FilePath $logFilePath -Append
+			$isoFileName = [System.IO.Path]::GetFileName($uri)
+			$webClient = New-Object System.Net.WebClient
+			$_date = Get-Date -Format hh:mmtt
+			$destinationFile = "$destination\$isoFileName"
+			$webClient.DownloadFile($uri, $destinationFile)
+			$_date = Get-Date -Format hh:mmtt
+			if((Test-Path $destinationFile) -eq $true) {
+				"Downloading ISO file succeeded at $_date" | Tee-Object -FilePath $logFilePath -Append
+				$result = $true
+			}
+			else {
+				"Downloading ISO file failed at $_date" | Tee-Object -FilePath $logFilePath -Append
+				$result = $false
+			}
+		} catch [Exception] {
+			"Failed to download ISO. Exception: $_" | Tee-Object -FilePath $logFilePath -Append
+			$retries--
+			if($retries -eq 0) {
+				Remove-Item $destination -Force -Confirm:0 -ErrorAction SilentlyContinue
+			}
+		}
+	}
+	
+	# Extract ISO
+	if($result)
+    {
+        "Mount the image from $destinationFile" | Tee-Object -FilePath $logFilePath -Append
+        $image = Mount-DiskImage -ImagePath $destinationFile -PassThru
+        $driveLetter = ($image | Get-Volume).DriveLetter
+
+        "Copy files to destination directory: $destination" | Tee-Object -FilePath $logFilePath -Append
+        Robocopy.exe ("{0}:" -f $driveLetter) $destination /E | Out-Null
+    
+        "Dismount the image from $destinationFile" | Tee-Object -FilePath $logFilePath -Append
+        Dismount-DiskImage -ImagePath $destinationFile
+    
+        "Delete the temp file: $destinationFile" | Tee-Object -FilePath $logFilePath -Append
+        Remove-Item -Path $destinationFile -Force
+
+        $txtOutput = @"
+Run this from command line for Certificates to be established on your server:
+
+wacs.exe --target manual --host mail.${domainName},owa.${domainName},autodiscover.${domainName} --store centralssl --centralsslstore "C:\CentralSSL" --installation iis,script --installationsiteid 1 --script "./Scripts/ImportExchange.ps1" --scriptparameters "'{CertThumbprint}' 'IIS,SMTP,IMAP' 1 '{CacheFile}' '{CachePassword}' '{CertFriendlyName}'" 
+"@ | out-file -filepath $installPath\README.txt -append -width 200
+
+        $runScript = $PSScriptRoot+"\aadccert.ps1"
+        &$runScript
+    }
+    else
+    {
+		"Failed to download the file after exhaust retry limit" | Tee-Object -FilePath $logFilePath -Append
+		Remove-Item $destination -Force -Confirm:0 -ErrorAction SilentlyContinue
+        Throw "Failed to download the file after exhaust retry limit"
+    }
+}
+
+DownloadISO
---
+++ Exchange/aadccert.ps1
@@ -0,0 +1,49 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$installPath = "$env:PUBLIC\Desktop\Hybrid"
+$SSLPath = "C:\CentralSSL"
+
+If(!(test-path $installPath))
+{
+      New-Item -ItemType Directory -Force -Path $installPath
+}
+
+If(!(test-path $SSLPath))
+{
+      New-Item -ItemType Directory -Force -Path $SSLPath
+}
+
+$outpath = "$installPath\1.AADConnect.exe"
+$url = "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+
+$outpath = "$installPath\ExchangeCertificatesTemp.zip"
+$url = "https://github.com/win-acme/win-acme/releases/download/v2.1.13.1/win-acme.v2.1.13.978.x64.pluggable.zip"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+
+$path = $installPath+"\2.ExchangeCertificates"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+Expand-Archive -Path $outpath -DestinationPath $path
+
+Remove-Item -Path $outpath
+
+$txtOutput = @"
+DOWNLOAD SUCCESSFUL!`n
+Please follow the guide on OneNote to continue:`n
+https://microsoft.sharepoint.com/teams/OfficePeople/_layouts/OneNote.aspx?id=%2Fteams%2FOfficePeople%2FSiteAssets%2FOffice%20People%20Notebook&wd=target%28Projects%2FTest%20Lab.one%7CA70B945C-4284-48F6-983A-30A0C55E78C1%2FExchange%202016%20Setup%20%2B%20Hybrid%20%26%20AADC%7C97D18BAD-BFC4-4C9E-92C5-AF81E1A13042%2F%29 `
+`n The Configuration/Installation order should be: `n
+1. Change DNS (If you haven't installed on Msfastlabs.com) `n
+2. Set up Exchange Certificates `n
+3. Set up Azure AD Connect `n
+4. Set up Exchange Hybrid `n
+"@
+
+$txtOutput | out-file -filepath $installPath\README.txt -append -width 200
+
+# disable IE Enhanced Security Configuration for administrators
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 -ErrorAction Stop
+
+Unblock-File -Path $path\Scripts\ImportExchange.ps1
